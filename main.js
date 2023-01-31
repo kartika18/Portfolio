2f243a9309b714ddd2eb308182f4162083e2a4f9
kartika18 <skartika426@gmail.com>
2023-01-31 17:34:25 +0700

    add contact

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function toggleDetail(element) {
-    const target = $(element.target)
+function toggleDetail(forEvent) {
+    const target = $(forEvent.target)
     const item = $(target).parents("#about-detail-exp")
     const detail = $(item).children("p")
 
@@ -9,5 +9,26 @@ function toggleDetail(element) {
         $(target).html("More Info").removeClass("active")
     } else {
         $(target).html("Less Info").addClass("active")
+    }
+}
+
+function onFormSubmit(forEvent) {
+    forEvent.preventDefault()
+
+    const email =$("#input-email")
+    const subject =$("#input-subject")
+    const message =$("#input-message")
+
+    if(!$(email).val()) {
+        alert("Email is Required")
+    } else if (!$(subject).val()) {
+        alert("Subject is Required")
+    } else if (!$(message).val()) {
+        alert("Message is Required")
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
     }
 }
